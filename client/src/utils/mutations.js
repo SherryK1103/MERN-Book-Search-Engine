70fadd5ab70e5680c2f3da0370efdf494928e722
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-11-11 19:20:45 -0600

    added add book and remove book queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,3 +26,41 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
