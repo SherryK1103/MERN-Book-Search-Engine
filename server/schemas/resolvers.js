46f6db00f3255c598bb051d197d48037f2fe3d55
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-11-12 18:43:46 -0600

    finished resolvers js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,8 +14,14 @@ const resolvers = {
         },
     },
     Mutation: {
-        login: async (parent, args, context) => {
-            const user = await User.findOne({ email: args.email });
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
             if (!user) {
                 throw new AuthenticationError('Not logged in');
             }
@@ -27,8 +33,35 @@ const resolvers = {
             }
             const token = signToken(user);
             return { token, user };
-        }
-    }
+        },
+
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: bookData } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    },
 };
 
 module.exports = resolvers;
